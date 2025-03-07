e928bca7d5cc280a4bf0c89652da28987f828648
Hernany Motoso <hernanymotoso@gmail.com>
2025-03-07 01:08:16 -0300

    refactor: create pool

--- src/server/api/routers/solana/pool/create-pool/index.ts
+++ src/server/api/routers/solana/pool/create-pool/index.ts
@@ -1,11 +1,9 @@
 import { env } from '@/env'
+import { getWhirlpoolProgram } from '@/server/api/program'
 import { publicProcedure } from '@/server/api/trpc'
 import { ResourceNotFoundError } from '@/utils/errors'
 import { RequiredFieldError } from '@/utils/errors/required-field-error'
 import { PriceMath } from '@/utils/math/price-math'
-import { AnchorProvider, Program } from '@coral-xyz/anchor'
-import { Whirlpool, WHIRLPOOL_PROGRAM_ID, WhirlpoolIDL } from '@project/anchor'
-import { AnchorWallet } from '@solana/wallet-adapter-react'
 import {
   clusterApiUrl,
   Connection,
@@ -39,33 +37,10 @@ export const createPool = publicProcedure
   )
   .mutation(async ({ input }) => {
     if (!input?.account) throw new RequiredFieldError('Account')
-
     const { account, tokenMintA, tokenMintB, tickSpacing, initialPrice } = input
-
-    const connection = new Connection(clusterApiUrl('devnet'))
     const sender = new PublicKey(account)
-
-    const dummyWallet = {
-      publicKey: sender,
-      signTransaction: () => {
-        throw new Error('Not implemented')
-      },
-      signAllTransactions: () => {
-        throw new Error('Not implemented')
-      },
-    }
-
-    const provider = new AnchorProvider(
-      connection,
-      dummyWallet as unknown as AnchorWallet,
-      { commitment: 'confirmed', skipPreflight: true },
-    )
-
-    const program = new Program(
-      WhirlpoolIDL as Whirlpool,
-      WHIRLPOOL_PROGRAM_ID,
-      provider,
-    )
+    const connection = new Connection(clusterApiUrl('devnet'))
+    const { program } = getWhirlpoolProgram(sender, connection)
 
     const feeTierPDA = buildFeeTierPDA(tickSpacing, program.programId)
     const feeTierAccount = await program.account.feeTier.fetch(feeTierPDA)
@@ -100,6 +75,7 @@ export const createPool = publicProcedure
       env.server.FUNDER_WALLET_SECRET_KEY,
     )
 
+    // TODO: save this pk
     const tokenVaultKeypair = Keypair.generate()
     const tokenVaultBKeypair = Keypair.generate()
 
@@ -119,8 +95,6 @@ export const createPool = publicProcedure
       rent: SYSVAR_RENT_PUBKEY,
       systemProgram: SystemProgram.programId,
     }
-
-    console.log('context\n', context)
 
     const instruction = await program.methods
       .initializePoolV2(tickSpacing, initialSqrtPrice)
@@ -142,14 +116,13 @@ export const createPool = publicProcedure
       tokenVaultKeypair,
       tokenVaultBKeypair,
     )
+
     const serializedTransaction = transaction
       .serialize({
         requireAllSignatures: false,
         verifySignatures: false,
       })
       .toString('base64')
-
-    console.log('serializedTransaction\n', serializedTransaction)
 
     return {
       serializedTransaction,
